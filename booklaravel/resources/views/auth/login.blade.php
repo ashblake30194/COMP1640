38928d405f9b6fdae4ecfd95363d95c2ff0846bb
NguyenNNS98 <44674668+NguyenNNS98@users.noreply.github.com>
2020-04-20 20:38:36 +0700

    Update Login page css

--- booklaravel/resources/views/auth/login.blade.php
+++ booklaravel/resources/views/auth/login.blade.php
@@ -1,73 +1,80 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Account Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+    <link rel="icon" type="image/png" href="{{asset('accountLogin/images/icons/favicon.ico')}}"/>
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('accountLogin/vendor/bootstrap/css/bootstrap.min.css')}}">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('accountLogin/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset('accountLogin/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
+<!--===============================================================================================-->  
+    <link rel="stylesheet" type="text/css" href="{{asset('accountLogin/css/util.css')}}">
+<!--===============================================================================================-->  
+    <link rel="stylesheet" type="text/css" href="{{asset('accountLogin/css/main.css')}}">
+<!--===============================================================================================-->
+</head>
+<body>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+<div class="limiter">
+    <div class="container-login100" style="background-image: url('accountLogin/images/bg-01.jpg');">
+        <div class="wrap-login100 p-t-30 p-b-50">
+            <span class="login100-form-title p-b-41">
+                {{ __('Login') }}
+            </span>
+            <p class="">
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </p>
+            <form class="login100-form validate-form p-b-33 p-t-5" method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="wrap-input100 validate-input">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror input100" 
+                            name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="User Email">
+                    <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+                    
                 </div>
-            </div>
+
+                <div class="wrap-input100 validate-input">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror input100" 
+                            name="password" required autocomplete="current-password" placeholder="Password">
+                    <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+                </div>
+
+                <div>
+                    <div style="text-align: center; padding-top:10px;">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}> &nbsp;{{ __('Remember Me') }}
+
+                        @if (Route::has('password.request'))
+                        <a class="btn btn-link" style="text-align: right;" href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a>
+                    @endif
+                    </div>
+                </div>
+
+                <div class="container-login100-form-btn m-t-32">
+                    <button type="submit" class="login100-form-btn">
+                        Login
+                    </button>
+                </div>
+
+            </form>
         </div>
     </div>
 </div>
-@endsection
+
+
+<div id="dropDownSelect1"></div>
+
+    <script src="{{asset('accountLogin/js/main.js')}}"></script>
+
+</body>
+</html>
